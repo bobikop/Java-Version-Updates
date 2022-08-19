48a24a750d82d028668c7f46653dd97e85ef9ee0
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-08-18 22:05:41 -0400

    comparator interface

--- m5-comparator_interface/src/main/java/com/cydeo/Main.java
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -10,21 +10,23 @@ public class Main {
         List<Integer> list = Arrays.asList(10,0,15,5,20);
         System.out.println(list);
 
-
+        //Ascending
         Collections.sort(list);
         System.out.println(list);
 
-
-
-//to sort list Java providing interface Comparator
-
         //Descending
-
-        Collections.sort(list, new MyComparator());
+        Collections.sort(list,new MyComparator());
         System.out.println(list);
-                               //same signature as abstract method bellow
-        Collections.sort(list, ((o1,o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));
 
+        Collections.sort(list,((o1,o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));
+
+        //Ascending order
+        list.sort((o1, o2) -> o1.compareTo(o2));
+        System.out.println(list);
+
+        //Descending order
+        list.sort((o2, o1) -> o1.compareTo(o2));
+        System.out.println(list);
 
 
 
