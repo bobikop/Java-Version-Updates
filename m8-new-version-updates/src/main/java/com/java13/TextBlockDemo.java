761ecbc234eb2a47fc52772cb157655cb8c7bb4b
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-08-23 14:59:08 -0400

    Java 13

---
+++ m8-new-version-updates/src/main/java/com/java13/TextBlockDemo.java
@@ -0,0 +1,30 @@
+package com.java13;
+
+public class TextBlockDemo {
+    public static void main(String[] args) {
+
+
+        String str = """
+                name: Boban Milosevic
+                Address: 15415 Beachwater Ct,
+                Dumfries, VA 22025
+                """;
+
+        System.out.println(str);
+
+        var address = "7925 Jones Branch Drive \n " +
+                "McLean,va 22102";
+
+        System.out.println(address);
+
+        var address2 = """
+                7925 Jones Branch Dr
+                McLean,VA 22102 
+                """;
+
+        System.out.println(address2);
+
+
+
+    }
+}
