43cfd069a4136dde49ab9c46177dab8cdb485533
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-05 11:29:05 -0400

    Java 14

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -3,16 +3,14 @@ package com.cydeo;
 public enum Currency {
     PENNY(1), NICKLE(5), DIME(10), QUARTER(25);  // this is objects of Currency, class ;
 
+    // assigning value to constant
     int value;
-
     Currency(int value) {
         this.value = value;
     }
-
     public int getValue() {
         return value;
     }
-
     void method(){
         System.out.println(1);
     }
