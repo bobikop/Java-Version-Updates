87ef39f0375b2a7a0986d9802e31514aee895988
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-05 12:07:58 -0400

    Java 14

--- m2-enum/src/main/java/com/cydeo/SwapValues/OddPredicate.java
+++ m2-enum/src/main/java/com/cydeo/SwapValues/OddPredicate.java
@@ -1,6 +1,7 @@
 package com.cydeo.SwapValues;
 
 public class OddPredicate implements UnaryPredicate<Integer>{
+    // test method here implements logic to check if number is ood
     public boolean test(Integer obj) {
         return obj % 2 != 0; // return whatever is in parameter
     }
--- m2-enum/src/main/java/com/cydeo/SwapValues/UnaryPredicate.java
+++ m2-enum/src/main/java/com/cydeo/SwapValues/UnaryPredicate.java
@@ -1,5 +1,5 @@
 package com.cydeo.SwapValues;
 
 public interface UnaryPredicate <T>{ //<> diamond token
-    public boolean test(T obj);
+    public boolean test(T obj);  // method accept object and return boolean
 }
