421e73cbadf9dd6ef5f45696ee6aa0e277be45d7
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-07 19:15:19 -0400

    Java 14 - Lambda

--- m3-lambda/src/main/java/com/cydeo/Sorting.java
+++ m3-lambda/src/main/java/com/cydeo/Sorting.java
@@ -1,6 +1,6 @@
 package com.cydeo;
 
 public interface Sorting {
-    public void sort();
+    public void sort(); // implementation of this method in separate class , for example quick sort, bubble sort...
 
 }
