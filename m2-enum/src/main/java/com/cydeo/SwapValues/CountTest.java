e2b6fd9f8ea8e9ac9fb25b56e4c016ed3733b8c7
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-06 20:53:03 -0400

    Java 14

--- m2-enum/src/main/java/com/cydeo/SwapValues/CountTest.java
+++ m2-enum/src/main/java/com/cydeo/SwapValues/CountTest.java
@@ -10,6 +10,9 @@ public class CountTest {
         int count = countIf(ci, new OddPredicate());
         System.out.println("Number of odd integers = " + count);
     }
+
+    // countIf method will be generic so method can accept other possible classes as well not just OddPredicate
+    // for example UnaryPredicate can accept oddPredicate or evenPredicate ....
     private static <T> int countIf(List<T> c, UnaryPredicate<T> p){
         int count = 0;
         for(T element : c){
