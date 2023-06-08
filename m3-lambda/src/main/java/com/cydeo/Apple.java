054a8821004aff152a763547f11be2dd69a564e3
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-08 09:38:28 -0400

    Java 14 - Lambda

--- m3-lambda/src/main/java/com/cydeo/Apple.java
+++ m3-lambda/src/main/java/com/cydeo/Apple.java
@@ -8,4 +8,8 @@ public class Apple {
 
     private int weight;
     private Color color;
+
+
+    // replacing code field for getter and to String method with above lombok annotation
+    // on this way we avoid so called boilerplate or verbose code.
 }
